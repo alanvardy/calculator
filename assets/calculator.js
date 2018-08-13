d8d3b78e6ed34a2e01680266b3a0fd71b4553cdd
Alan Vardy <alanvardy@gmail.com>
2018-08-12 21:05:18 -0600

    cleaned up code

--- assets/calculator.js
+++ assets/calculator.js
@@ -8,20 +8,59 @@ var operatorDisplay = document.querySelector(".operatorDisplay");
 
 function press(button) { //function routing for both mouse and keyboard input
     switch (button) {
-        case "AC": button_ac(); flash("ac"); break;
-        case "Del": button_del(); flash("del"); break;
-        case "1/x": button_1x(); flash("inverse"); break;
-        case "MC": button_mc(); flash("mc"); break;
-        case "M+": button_mplus(); flash("mplus"); break;
-        case "M-": button_mminus(); flash("mminus"); break;
-        case "MR": button_mr(); flash("mr"); break;
-        case "/": button_divide(); flash("divide"); break;
-        case "x": button_multiply(); flash("multiply"); break;
-        case "-": button_subtract(); flash("subtract"); break;
-        case "+": button_add(); flash("add"); break;
-        case "=": button_equals(); flash("equals"); break;
-        default: number(button); flash("b" + button); break;
-        }
+        case "AC":
+            button_ac();
+            flash("ac");
+            break;
+        case "Del":
+            button_del();
+            flash("del");
+            break;
+        case "1/x":
+            button_1x();
+            flash("inverse");
+            break;
+        case "MC":
+            button_mc();
+            flash("mc");
+            break;
+        case "M+":
+            button_mplus();
+            flash("mplus");
+            break;
+        case "M-":
+            button_mminus();
+            flash("mminus");
+            break;
+        case "MR":
+            button_mr();
+            flash("mr");
+            break;
+        case "/":
+            button_divide();
+            flash("divide");
+            break;
+        case "x":
+            button_multiply();
+            flash("multiply");
+            break;
+        case "-":
+            button_subtract();
+            flash("subtract");
+            break;
+        case "+":
+            button_add();
+            flash("add");
+            break;
+        case "=":
+            button_equals();
+            flash("equals");
+            break;
+        default:
+            number(button);
+            flash("b" + button);
+            break;
+    }
 }
 
 function flash(id) { //for screen refresh and button press effects
@@ -30,7 +69,7 @@ function flash(id) { //for screen refresh and button press effects
     display.classList.add("flash");
     operatorDisplay.classList.add("flash");
     flashButton.classList.add("black");
-    setTimeout(function() {
+    setTimeout(function () {
         display.classList.remove("flash");
         operatorDisplay.classList.remove("flash");
         flashButton.classList.remove("black");
@@ -58,8 +97,10 @@ function button_ac() {
 }
 
 function button_del() {
-    display.textContent = display.textContent.slice(0,-1);
-    if (display.textContent == "") { display.textContent = "0";}
+    display.textContent = display.textContent.slice(0, -1);
+    if (display.textContent == "") {
+        display.textContent = "0";
+    }
 }
 
 function button_1x() {
@@ -120,57 +161,137 @@ function number(num) { //handles all digits and period
     } else {
         display.textContent = display.textContent + num;
     }
-    if (display.textContent == ".") {display.textContent = "0.";}
-    
+    if (display.textContent == ".") {
+        display.textContent = "0.";
+    }
+
 }
 
-buttons.forEach( function(button) {
-    button.addEventListener("click", function() {
+buttons.forEach(function (button) {
+    button.addEventListener("click", function () {
         press(button.textContent, button);
     });
 });
 
-document.onkeyup = function(e) { //keyboard functionality
+document.onkeyup = function (e) { //keyboard functionality
     switch (e.which) {
-        case 49: press('1'); break;
-        case 97: press('1'); break;
-        case 50: press('2'); break;
-        case 98: press('2'); break;
-        case 51: press('3'); break;
-        case 99: press('3'); break;
-        case 52: press('4'); break;
-        case 100: press('4'); break;
-        case 53: press('5'); break;
-        case 101: press('5'); break;
-        case 54: press('6'); break;
-        case 102: press('6'); break;
-        case 55: press('7'); break;
-        case 103: press('7'); break;
-        case 56: press('8'); break;
-        case 104: press('8'); break;
-        case 57: press('9'); break;
-        case 105: press('9'); break;
-        case 48: press('0'); break;
-        case 96: press('0'); break;
-        case 190: press('.'); break;
-        case 110: press('.'); break;
-        case 65: press("AC"); break;
-        case 67: press("AC"); break;
-        case 46: press("Del"); break;
-        case 83: press("1/x"); break;
-        case 81: press("MC"); break;
-        case 87: press("M+"); break;
-        case 69: press("M-"); break;
-        case 82: press("MR"); break;
-        case 19: press("/"); break;
-        case 111: press("/"); break;
-        case 88: press("x"); break;
-        case 106: press("x"); break;
-        case 109: press("-"); break;
-        case 189: press("-"); break;
-        case 107: press("+"); break;
-        case 187: press("+"); break;
-        case 13: press("="); break;
+        case 49:
+            press('1');
+            break;
+        case 97:
+            press('1');
+            break;
+        case 50:
+            press('2');
+            break;
+        case 98:
+            press('2');
+            break;
+        case 51:
+            press('3');
+            break;
+        case 99:
+            press('3');
+            break;
+        case 52:
+            press('4');
+            break;
+        case 100:
+            press('4');
+            break;
+        case 53:
+            press('5');
+            break;
+        case 101:
+            press('5');
+            break;
+        case 54:
+            press('6');
+            break;
+        case 102:
+            press('6');
+            break;
+        case 55:
+            press('7');
+            break;
+        case 103:
+            press('7');
+            break;
+        case 56:
+            press('8');
+            break;
+        case 104:
+            press('8');
+            break;
+        case 57:
+            press('9');
+            break;
+        case 105:
+            press('9');
+            break;
+        case 48:
+            press('0');
+            break;
+        case 96:
+            press('0');
+            break;
+        case 190:
+            press('.');
+            break;
+        case 110:
+            press('.');
+            break;
+        case 65:
+            press("AC");
+            break;
+        case 67:
+            press("AC");
+            break;
+        case 46:
+            press("Del");
+            break;
+        case 83:
+            press("1/x");
+            break;
+        case 81:
+            press("MC");
+            break;
+        case 87:
+            press("M+");
+            break;
+        case 69:
+            press("M-");
+            break;
+        case 82:
+            press("MR");
+            break;
+        case 19:
+            press("/");
+            break;
+        case 111:
+            press("/");
+            break;
+        case 88:
+            press("x");
+            break;
+        case 106:
+            press("x");
+            break;
+        case 109:
+            press("-");
+            break;
+        case 189:
+            press("-");
+            break;
+        case 107:
+            press("+");
+            break;
+        case 187:
+            press("+");
+            break;
+        case 13:
+            press("=");
+            break;
     }
 
 };
